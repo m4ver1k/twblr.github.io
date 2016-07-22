f0f1fe9bb7aaecea710b2d8d18b3eea742138eb5
m4ver1k <m4ver1k@machina>
2016-07-22 16:10:23 +0530

    collatzChainLength - updated for condition

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -5,16 +5,13 @@ func collatzChainLength(n int) int {
 		//odd 3n+1
 		count :=0;
 
-		for {
+		for n!=1{
 					if n % 2==0{
 						n=n/2
 						count = count +1
 					}else{
 						n=(n*3)+1
 						count = count +1
-					}
-					if(n==1){
-						break
 					}
 			}
 			return count
