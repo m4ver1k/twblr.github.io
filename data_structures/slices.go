7bceb4fa4e7ab30b45b3810181c41741fcadb8e5
m4ver1k <m4ver1k@machina>
2016-07-22 22:14:57 +0530

    Golang slice example

--- data_structures/slices.go
+++ data_structures/slices.go
@@ -4,22 +4,53 @@ type mapOperation func(int32) int32
 type filterOperation func(int32) bool
 
 func mapInts(op mapOperation, vals []int32) []int32 {
-	return []int32{}
+
+ result :=[]int32{}
+	for _,i := range vals{
+		result=append(result, op(i))
+	}
+return result
 
 }
 
 func filterInts(op filterOperation, vals []int32) []int32 {
-	return []int32{3}
+
+	 result :=[]int32{}
+		for _,i := range vals{
+				if(op(i)){
+								result=append(result, i)
+				}
+		}
+	return result
+
 }
 
 func concatenate(dest []string, newValues ...string) []string {
-	return nil
+  result :=make([]string, len(dest))
+  copy(result,dest)
+  result = append(result,newValues...)
+  return result
 }
 
 func equals(list1 []string, list2 []string) bool {
-	return false
+  if len(list1)!=len(list2){
+    return false
+  }
+  for index,item := range list1{
+    if item != list2[index]{
+      return false
+    }
+  }
+	return true;
 }
 
 func partialReverse(src []int, from, to int) []int {
-	return nil
+  result := []int{}
+  if to > len(src){
+    to = len(src)
+  }
+  for i :=to;i>=from;i-- {
+    result=append(result,src[i])
+  }
+	return result
 }
