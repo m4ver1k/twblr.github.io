644c6f9244c752167f87b7015a38005fc620ee4b
m4ver1k <m4ver1k@machina>
2016-07-23 00:05:28 +0530

    singly linked list

--- data_structures/structs.go
+++ data_structures/structs.go
@@ -1,13 +1,34 @@
 package data_structures
-
 // Linked List Implementation
 
-type Node struct{}
-
+type Node struct{
+	data int
+	next *Node
+}
+var top *Node
 func Add(elem int) {
+	var cur *Node
+
+	if top == nil{
+		top = &Node{data:elem}
+	}else{
+		cur = top
+		for cur.next != nil{
+			cur = cur.next
+		}
+		cur.next = &Node{data:elem}
+	}
 	return
 }
 
 func Search(elem int) *Node {
+	cur  := top
+	for cur !=nil{
+		if cur.data==elem{
+			return cur
+		}else{
+			cur=cur.next
+		}
+	}
 	return nil
 }
