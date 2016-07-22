4573415b89f0d49376878bf7f28d6d6d5c0b8b13
m4ver1k <m4ver1k@machina>
2016-07-22 16:07:10 +0530

    collatzChainLength

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,22 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+		// even /2
+		//odd 3n+1
+		count :=0;
+
+		for {
+					if n % 2==0{
+						n=n/2
+						count = count +1
+					}else{
+						n=(n*3)+1
+						count = count +1
+					}
+					if(n==1){
+						break
+					}
+			}
+			return count
+
 }
