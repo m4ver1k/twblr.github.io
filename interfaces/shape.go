f26eb5059661c5f99a0ca4dacb4f4a61793c7837
m4ver1k <m4ver1k@machina>
2016-07-23 10:29:31 +0530

    Interafeces example

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -3,3 +3,29 @@ package interfaces
 type Shape interface {
 	Area() int
 }
+
+
+type Square struct{
+	side int
+}
+
+func (this Square) Area() int{
+	return this.side * this.side
+}
+
+type Rectangle struct{
+	length int
+	breadth int
+}
+
+func (this Rectangle) Area() int{
+	return this.length * this.breadth
+}
+type Hybrid struct{
+	 s1 Shape
+	 s2 Shape
+ }
+
+func (this Hybrid) Area() int{
+	return this.s1.Area() + this.s2.Area()
+}
